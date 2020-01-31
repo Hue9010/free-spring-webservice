ded4ba72082c166662b295506f5d739023fb6dec
이순환 <preprofession@gmail.com>
2020-01-31 17:03:49 +0900

    Initial commit

---
+++ src/main/java/com/ecycle/book/freespringwebservice/FreeSpringWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ecycle.book.freespringwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeSpringWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeSpringWebserviceApplication.class, args);
+	}
+
+}
